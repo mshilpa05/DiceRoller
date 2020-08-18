3570ca340b1be5d34a1fd796a7e26269a8c9c1d4
shilpa mathew <mshilpa0596@gmail.com>
2020-08-18 12:36:39 +1200

    App developed to roll two dice at a time

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,62 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Text('Dicee',style: TextStyle(fontFamily: 'Pacifico',color: Colors.white,fontWeight:FontWeight.bold)),
+          backgroundColor: Colors.blueGrey,
         ),
-        body: DicePage(),
+        body: DiceGame(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DiceGame extends StatefulWidget {
+  @override
+  _DiceGameState createState() => _DiceGameState();
+}
+
+class _DiceGameState extends State<DiceGame> {
+  int leftdicenumber=1;
+  int rightdicenumber=1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDiceNumber();
+                print("Left Button Got pressed");
+              },
+              child: Image.asset('images/dice$leftdicenumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDiceNumber();
+                print("Right button got pressed");
+              },
+              child: Image.asset('images/dice$rightdicenumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void changeDiceNumber(){
+    setState(() {
+      rightdicenumber=Random().nextInt(6)+1;
+      leftdicenumber=Random().nextInt(6)+1;
+    });
   }
 }
+
